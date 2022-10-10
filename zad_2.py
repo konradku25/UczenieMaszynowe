4200fe1a3e2d7318b402bfd56445e7666bf0b60e
Konrad Kula <konradku25@gmail.com>
2022-10-10 14:09:52 +0200

    Dodanie zadania 2

---
+++ zad_2.py
@@ -0,0 +1,53 @@
+
+#Zadanie 2a
+def Zadanie2a(imie1,imie2,imie3,imie4,imie5):
+    print("{} {} {} {} {}".format(imie1,imie2,imie3,imie4,imie5))
+
+Zadanie2a("Sebastian","Esteban","Fernando","Sergio","Nyck")
+
+#Zadanie 2b
+def Zadanie2bI(lista):
+    for x in range(0,len(lista)):
+        lista[x]=lista[x]*2
+    return lista
+
+liczby=[1,2,3,4,5]
+print(Zadanie2bI(liczby))
+
+def Zadanie2bII(lista):
+    lista=[2*x for x in lista]
+    return lista
+
+print(Zadanie2bII(liczby))
+
+#Zadanie 2c
+liczby2=[]
+for x in range (0,10):
+    liczby2.append(x)
+
+def Zadanie2c(lista):
+    for x in lista:
+        if x%2==0:
+            if x!=0:
+                print(x)
+
+Zadanie2c(liczby2)
+
+#Zadanie 2d
+def Zadanie2d(lista):
+    for x in range(0,len(lista),2):
+        print(x)
+
+Zadanie2d(liczby2)
+
+
+
+
+
+
+
+
+
+
+
+
