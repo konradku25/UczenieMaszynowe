1d17b7e727335d5131b7873d3117877bfe27c50c
konradku25 <konradku25@Gmail.com>
2022-11-06 18:04:31 +0100

    feat: added

--- zad_2.py
+++ zad_2.py
@@ -1,22 +1,18 @@
-import funcs as f
+import pytesseract
+import cv2
+import matplotlib.pyplot as plt
 
-# Zadanie 2a
-imiona = ["Sebastian", "Esteban", "Fernando", "Sergio", "Nyck"]
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
-f.Zadanie2a(imiona)
 
-# Zadanie 2b
-liczby = [1, 2, 3, 4, 5]
-print(f.Zadanie2bI(liczby))
+def img_text(img_str):
+    img = cv2.imread(img_str)
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    print(pytesseract.image_to_string(img_gray))
 
-print(f.Zadanie2bII(liczby))
 
-# Zadanie 2c
-liczby2 = []
-for x in range(0, 10):
-    liczby2.append(x)
-
-f.Zadanie2c(liczby2)
-
-# Zadanie 2d
-f.Zadanie2d(liczby2)
+img_text("images\\img1.png")
+img_text("images\\img2.png")
+img_text("images\\img3.png")
+img_text("images\\img4.png")
+img_text("images\\img5.png")
